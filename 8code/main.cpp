5da3b9c3c83c3655815279da2916220f920ad64a
刘丞之 <user@usertekimacbook-pro.local>
2016-04-24 15:55:36 +0800

    Initial Commit

---
+++ 8code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  8code
+//
+//  Created by 丞之 on 16/4/24.
+//  Copyright © 2016年 丞之. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
