a73f75a38f044acb12274a8db0c6e48cdcf11510
丞之 <zmq175@qq.com>
2016-04-26 17:02:03 +0800

    8code Problem solving

---
+++ build/8code.build/Debug/8code.build/Objects-normal/x86_64/Deep.d
@@ -0,0 +1,2 @@
+dependencies: /Users/user/Documents/8code/8code/Deep.cpp \
+  /Users/user/Documents/8code/8code/Deep.h
---
+++ build/8code.build/Debug/8code.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,2 @@
+dependencies: /Users/user/Documents/8code/8code/main.cpp \
+  /Users/user/Documents/8code/8code/Deep.h
